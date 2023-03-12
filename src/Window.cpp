9c4114ff90a43ccbf56c1c6813c6d0ad53787581
Kevin Wu <28658818+ypwk@users.noreply.github.com>
2023-03-12 13:32:50 -0400

    addon to previous commit

--- src/Window.cpp
+++ src/Window.cpp
@@ -41,7 +41,7 @@ bool Window::Create()
 #endif
 
     // Create window with graphics context
-    window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
+    window = glfwCreateWindow(1280, 720, "surface code go pew pew", NULL, NULL);
     if (window == NULL)
         return 1;
     glfwMakeContextCurrent(window);
