561ad7206024bc16afc27491403eec6b5c86e80b
Kevin Wu <28658818+ypwk@users.noreply.github.com>
2023-04-21 18:22:01 -0400

    Added VirtualStabillizer functionality

--- decoder/render/src/ig_contentwindow.cpp
+++ decoder/render/src/ig_contentwindow.cpp
@@ -42,7 +42,7 @@ void ImGui_ContentWindowHandler::RenderInit()
     GLCall(glBindTexture(GL_TEXTURE_2D, 0));
 
     // attach texture to fbo 
-    GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_TXT, 0));
+    GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, m_TXT, 0));
 }
 
 void ImGui_ContentWindowHandler::PreRender()
